4dbb254f6d1d69885fcf0188fc9f4114961221df
ngbao12 <ngocbao6422@gmail.com>
2024-11-24 23:33:51 +0700

    implement playButton, settingButton, infor Button and score Button

--- src/levels/GUI.hpp
+++ src/levels/GUI.hpp
@@ -16,6 +16,11 @@
 #include <functional>
 #include <stdint.h>
 
+
+Font FONT = LoadFont(<#const char *fileName#>)
+    
+};
+
 class Button {
     protected:
         Rectangle outerRect;
@@ -28,13 +33,31 @@ class Button {
     public:
         Button(Rectangle rect, const std::string text, float yText, Color textColor, float fontSize, Font font = FONT);
         Button() : Button({0,0,0,0}, "", 0.0f, BLUE, 0) {}
-        void draw(float radius = 15);
+        virtual void draw(float radius = 15);
         int handle();
         bool getIsHovered() {return this->isHovered;}
         
         virtual ~Button() = default;
 };
 
+class PlayButton : public Button {
+public:
+    void draw(float radius = 15);
+};
 
+class SettingButton : public Button {
+public:
+    void draw(float radius = 15);
+};
+
+class HighScoreButton : public Button {
+public:
+    void draw(float radius = 15);
+};
+
+class InforButton : public Button {
+public:
+    void draw(float radius = 15);
+};
 
 #endif /* GUI_h */
