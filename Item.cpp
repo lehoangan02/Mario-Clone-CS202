5826628eb4b6a8ab6ab7e7133d42c105d0d177a9
ntd <lqd.tk22.ngotandat@gmail.com>
2024-11-25 18:05:09 +0700

    Add folder Item

---
+++ Item.cpp
@@ -0,0 +1,40 @@
+#include "Item.h"
+#include <iostream>
+Item::Item(Vector2 pos, Vector2 size, Texture2D tex, int totalFrames, float switchTime)
+	: position(pos), size(size), texture(tex), totalFrames(totalFrames), switchTime(switchTime) {
+	currentFrame = 0;
+	elapsedTime = 0.0f;
+	frameSize = { (float)(tex.width / totalFrames), (float)tex.height };
+	uvRect = { 0, 0, frameSize.x, frameSize.y };
+}
+Item::~Item() {
+	UnloadTexture(texture);
+}
+void Item::Update(float deltaTime) {
+	elapsedTime += deltaTime;
+
+	if (elapsedTime >= switchTime) {
+		elapsedTime = 0.0f;
+		currentFrame = (currentFrame + 1) % totalFrames;
+		uvRect.x = currentFrame * frameSize.x;
+	}
+}
+void Item::Draw() {
+	DrawTextureRec(texture, uvRect, position, WHITE);
+}
+Coin::Coin(Vector2 pos, Vector2 size, Texture2D tex) 
+	: Item(pos, size, tex, COIN_FRAME_COUNT, COIN_FRAME_TIME) {}
+void Coin::applyEffect(Character* character) {
+	return;
+}
+Mushroom::Mushroom(Vector2 pos, Vector2 size, Texture2D tex) 
+	: Item(pos, size, tex, MUSHROOM_FRAME_COUNT, MUSHROOM_FRAME_TIME) {}
+void Mushroom::applyEffect(Character* character) {
+	return;
+}
+FireFlower::FireFlower(Vector2 pos, Vector2 size, Texture2D tex) 
+	: Item(pos, size, tex, FIREFLOWER_FRAME_COUNT, FIREFLOWER_FRAME_TIME) {}
+void FireFlower::applyEffect(Character* character) {
+	return;
+}
+
---
+++ Item.h
@@ -0,0 +1,61 @@
+#pragma 
+
+constexpr int COIN_FRAME_COUNT = 4;
+constexpr float COIN_FRAME_TIME = 0.1f;
+
+constexpr int MUSHROOM_FRAME_COUNT = 6;
+constexpr float MUSHROOM_FRAME_TIME = 0.08f;
+
+constexpr int FIREFLOWER_FRAME_COUNT = 5;
+constexpr float FIREFLOWER_FRAME_TIME = 0.12f;
+
+#include "src/animation/Animation.h"
+#include "src/characters/Character.h"
+
+
+
+class Item {
+protected:
+	Vector2 position;
+	Vector2 size;
+	Texture2D texture;
+	Rectangle uvRect;  // Khu vuc texture hien thi
+	Vector2 frameSize;
+	int totalFrames;
+	int currentFrame;
+	float switchTime;  // Thoi gian chuyen doi giua cac frame
+	float elapsedTime; // Thoi gian troi qua tu lan doi frame truoc
+public:
+	Item(Vector2 pos, Vector2 size, Texture2D texture, int totalFrames, float switchTime);
+	virtual ~Item();
+	virtual void applyEffect(Character* character) = 0;
+	virtual void Update(float deltaTime);
+	virtual void Draw();
+
+	Vector2 GetPosition() const {
+		return position;
+	}
+	Vector2 GetSize() const {
+		return size;
+	}
+
+};
+class Coin : public Item {
+public:
+	Coin(Vector2 pos, Vector2 size, Texture2D tex);
+	void applyEffect(Character* Character) override;
+
+};
+class Mushroom : public Item {
+public:
+	Mushroom(Vector2 pos, Vector2 size, Texture2D tex);
+	void applyEffect(Character* character);
+
+};
+class FireFlower : public Item {
+public:
+	FireFlower(Vector2 pos, Vector2 size, Texture2D tex);
+	void applyEffect(Character* character);
+};
+
+
