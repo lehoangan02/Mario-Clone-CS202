e5c22e59e24273942909816fa6c746c4a2cde973
ngbao12 <ngocbao6422@gmail.com>
2024-12-16 18:56:15 +0700

    goomba big

--- src/Mario/main.cpp
+++ src/Mario/main.cpp
@@ -18,7 +18,9 @@ int main() {
     InitWindow(1024, 768, "Game Window");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     
-    ShyGuy shy({300, 300}, {42, 58}, {10,0}, 100, 700, 600, 900);
+    Goomba goomba({300, 300}, {60, 60}, {10,0});
+    goomba.setBound(100, 500, 100, 500);
+    //ShyGuy shy({300, 300}, {42, 58}, {10,0}, 100, 700, 600, 900);
     while(!WindowShouldClose()) {
         BeginDrawing();
         ClearBackground(RAYWHITE);
@@ -27,16 +29,16 @@ int main() {
 
         // DrawRectangleLines(goomba.getBoundingBox().x, goomba.getBoundingBox().y, goomba.getBoundingBox().width,  goomba.getBoundingBox().height,BLUE);
         // goomba.test();
-        // goomba.update(0.01f);
-        // goomba.render();
+        goomba.update(0.01f);
+        goomba.render();
         // DrawRectangleLines(goomba2.getBoundingBox().x, goomba2.getBoundingBox().y, goomba2.getBoundingBox().width,  goomba2.getBoundingBox().height,BLUE);
 
         // goomba2.test();
         // goomba2.update(0.01f);
         // goomba2.render();
-        DrawLineEx({700,100}, {700,400}, 2.0f, RED);
-        shy.update(0.01f);
-        shy.render();
+        //DrawLineEx({700,100}, {700,400}, 2.0f, RED);
+        //shy.update(0.01f);
+        //shy.render();
         EndDrawing();
         
     }
