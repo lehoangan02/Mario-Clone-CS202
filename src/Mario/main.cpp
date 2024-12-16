51929944d4251c70a0eb9ec56dc3a41e3ed2d98d
Chowkey <lekimchau211@gmail.com>
2024-12-16 21:04:50 +0700

    adding file into sln file

--- src/Mario/main.cpp
+++ src/Mario/main.cpp
@@ -1,6 +1,7 @@
 #include "../Application.hpp"
 
 int main() {
+	SetTargetFPS(60);
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(1024, 768, "Game Window");
     SetWindowState(FLAG_WINDOW_RESIZABLE);
