87188c72ee5112a1e33df9fa6b9b0fbc9c980cb0
lehoangan02 <146849112+lehoangan02@users.noreply.github.com>
2024-12-16 18:06:51 +0700

    Ammend Merge branch 'ngocbao' into lehoangan02

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,7 +2,7 @@
 
 Game::Game() 
     : factory(LevelFactory::GetLevelFactory()), 
-      level(factory.CreateLevel(LevelFactory::LEVEL_TESTING)),
+      level(factory.CreateLevel(LevelFactory::LEVEL_101)),
       character(ResourceManager::GetInstance()->GetTexture("mario"))
 {
     // character = ResourceManager::GetInstance()->GetTexture("mario");
@@ -11,7 +11,6 @@ Game::Game()
     std::cout << "Level Type: " << level -> GetLevelType() << std::endl;
     player->setPosition(Vector2{20, 0});  
     level->attachPlayer(player);  
-    std::cout << "Level Type: " << level -> GetLevelType() << std::endl;
 }
 
 Game::Game(int characterMenu, int levelMenu) 
@@ -19,7 +18,7 @@ Game::Game(int characterMenu, int levelMenu)
       level(nullptr)  
 {
     if (levelMenu == 0) {
-        level = factory.CreateLevel(LevelFactory::LEVEL_101);
+        level = factory.CreateLevel(LevelFactory::LEVEL_103);
     }
     else if (levelMenu == 1) {
         level = factory.CreateLevel(LevelFactory::LEVEL_102);
@@ -70,26 +69,133 @@ Game& Game::operator=(const Game& other) {
     return *this; 
 }
 void Game::start() {
-    SetTargetFPS(60);  
     while (!WindowShouldClose()) {
-        float deltaTime = GetFrameTime();  
-        
-        update(deltaTime);  
-        draw();  
+        update(GetFrameTime());
+        draw();
+        if (IsKeyDown(KEY_N)) {
+            nextLevel();
+        }
+        else if (IsKeyDown(KEY_O)) {
+            hiddenLevel();
+        }
     }
-
-    CloseWindow();  
+    CloseWindow();
 }
 
 void Game::update(float deltaTime) {
-    level->update(deltaTime);  
+    if (level) {
+        level->update(deltaTime);
+    }
 }
 
 void Game::draw() {
     BeginDrawing();
-    ClearBackground(WHITE);
-
-    level->render();  
-
+    ClearBackground(RAYWHITE);
+    if (level) {
+        level->render();
+    }
     EndDrawing();
+}
+
+
+
+void Game::notify(Component* sender, int eventCode) {
+    switch (eventCode) {
+        case 0:
+            state = LEVEL_RETURN_MESSAGE::PAUSE;
+            break;
+        case 1:
+            state = LEVEL_RETURN_MESSAGE::CONTINUE;
+            break;
+        case 2:
+            state = LEVEL_RETURN_MESSAGE::RUNNING;
+            break;
+        case 3:
+            state = LEVEL_RETURN_MESSAGE::HIDDEN;
+            break;
+        case 4:
+            state = LEVEL_RETURN_MESSAGE::WIN;
+            break;
+        case 5:
+            state = LEVEL_RETURN_MESSAGE::LOSE;
+            break;
+        case 6:
+            state = LEVEL_RETURN_MESSAGE::QUIT;
+            break;
+        default:
+            break;
+    }
+}
+
+void Game::pause() {
+    notify(nullptr, 0); // Pause event
+}
+
+void Game::resume() {
+    notify(nullptr, 1); // Continue event
+}
+
+void Game::win() {
+    notify(nullptr, 4); // Win event
+}
+
+void Game::lose() {
+    notify(nullptr, 5); // Lose event
+}
+
+void Game::quit() {
+    notify(nullptr, 6); // Quit event
+}
+
+void Game::nextLevel() {
+    // Logic to switch to the next level
+    if (level->GetLevelType() == LevelFactory::LEVEL_101) {     
+        level = factory.CreateLevel(LevelFactory::LEVEL_103);
+    // } else if (level->GetLevelType() == LevelFactory::LEVEL_102) {
+    //     level = factory.CreateLevel(LevelFactory::LEVEL_103);
+    } else {
+    }
+    player->setPosition(Vector2{20, 0});
+    level->update(0.0f);
+    level->attachPlayer(player);
+    state = LEVEL_RETURN_MESSAGE::RUNNING; 
+}
+
+void Game::hiddenLevel() {
+    if (level->GetLevelType() == LevelFactory::LEVEL_101) 
+        level = factory.CreateLevel(LevelFactory::LEVEL_103);
+    // else if (level->GetLevelType() == LevelFactory::LEVEL_102) 
+    //     level = factory.CreateLevel(LevelFactory::LEVEL_103);
+    else if (level->GetLevelType() == LevelFactory::LEVEL_103) 
+        level = factory.CreateLevel(LevelFactory::LEVEL_103);
+    player->setPosition(Vector2{20, 0});
+    level->update(0.0f);
+    level->attachPlayer(player);
+    state = LEVEL_RETURN_MESSAGE::RUNNING; 
+}
+
+void Game::handleState() {
+    switch (state) {
+        case LEVEL_RETURN_MESSAGE::PAUSE:
+            //drawPauseMenu();
+            break;
+        case LEVEL_RETURN_MESSAGE::CONTINUE:
+            // Handle continue state
+            break;
+        case LEVEL_RETURN_MESSAGE::RUNNING:
+            // Handle running state
+            break;
+        case LEVEL_RETURN_MESSAGE::HIDDEN:
+            hiddenLevel();
+            break;
+        case LEVEL_RETURN_MESSAGE::WIN:
+            nextLevel();
+            break;
+        case LEVEL_RETURN_MESSAGE::LOSE:
+            //drawLoseButton();
+            break;
+        case LEVEL_RETURN_MESSAGE::QUIT:
+            //drawQuitButton();
+            break;
+    }
 }
--- src/characters/Enemy.cpp
+++ src/characters/Enemy.cpp
@@ -57,18 +57,57 @@ void Enemy::render() {
 Goomba::Goomba(Vector2 position) : Enemy(position) {
     this->position = position;
     this->originPosition = position;
+    
     texture = LoadTexture("assets/textures/Goomba_Walk1.png");
     textures.push_back(LoadTexture("assets/textures/Goomba_Walk1.png"));
     textures.push_back(LoadTexture("assets/textures/Goomba_Walk2.png"));
     textures.push_back(LoadTexture("assets/textures/Goomba_Flat.png"));
-    size = { 32, 32 };
+
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[2], TEXTURE_FILTER_POINT);
+
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[2], TEXTURE_WRAP_CLAMP);
+    size = { 16, 16 };
     speed = { 40, 0 };
     isRight = false;
     isDown = false;
     isDead = false;
     isDying = false;
+    isCollisionTrue = false;
 }
 
+Goomba::Goomba(Vector2 position, Vector2 size, Vector2 speed) : Enemy(position) {
+    this->position = position;
+    this->originPosition = position;
+    
+    texture = LoadTexture("assets/textures/Goomba_Walk1.png");
+    textures.push_back(LoadTexture("assets/textures/Goomba_Walk1.png"));
+    textures.push_back(LoadTexture("assets/textures/Goomba_Walk2.png"));
+    textures.push_back(LoadTexture("assets/textures/Goomba_Flat.png"));
+
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[2], TEXTURE_FILTER_POINT);
+
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[2], TEXTURE_WRAP_CLAMP);
+
+    this->size = size;
+    this->speed = speed;
+    isRight = false;
+    isDown = false;
+    isDead = false;
+    isDying = false;
+    isCollisionTrue = false;
+}
 void Goomba::hit() {
     if (isCollisionTrue) {
         this->texture = textures[2];           
@@ -103,10 +142,10 @@ void Goomba::update(float deltaTime) {
         currentTextureIndex = (currentTextureIndex + 1) % 2; 
         texture = textures[currentTextureIndex];
     }
-    if (position.x < leftBound || position.x + texture.width > rightBound) {
+    if (position.x < leftBound || position.x + texture.width * size.x/16 > rightBound) {
         isRight = !isRight;
     }
-    if (position.y > bottomBound) {
+    if (position.y + size.y> bottomBound) {
         isDown = false;
     }
 
@@ -116,29 +155,293 @@ void Goomba::update(float deltaTime) {
 }
 
 void Goomba::render() {
-    if (!isDead) DrawTexture(texture, position.x, position.y, WHITE);
+    if (!isDead) DrawTextureEx(texture, position, 0.0f, size.x/16, WHITE);
 }
 
-void Goomba::test() {
-    if(IsKeyDown(KEY_A)) {
-        accelerate(0.1f);
-    }
-    if (IsKeyDown(KEY_B)) {
-        flipDirection();
-    }
-    if (IsKeyDown(KEY_C)) {
-        hit();
-    }
-    if (IsKeyDown(KEY_D)) {
-        resetSpeedY();
-    }
-    if (IsKeyDown(KEY_E)) {
-        setDirection(slidingDirection::right);
-    }
-    if (IsKeyDown(KEY_F)) {
-        setDirection(slidingDirection::down);
-    }
-    if (IsKeyDown(KEY_G)) {
-        setDirection(slidingDirection::left);
-    }  
+
+PiranhaPlant::PiranhaPlant(Vector2 position) : Enemy(position) {
+    this->position = position;
+    this->originPosition = position;
+    
+    texture = LoadTexture("assets/textures/PiranhaPlant_0.png");
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+
+    textures.push_back(LoadTexture("assets/textures/PiranhaPlant_0.png"));
+    textures.push_back(LoadTexture("assets/textures/PiranhaPlant_1.png"));
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+
+    size = { 32, 66 };
+    speed = { 0, 10};
+    isRight = false;
+    isDown = false;
+    isDead = false;
+    isPauseCollision = false;
+
+    heightInGround = 0;
+    topBound = size.y + position.y;
+    bottomBound = position.y;
 }
+
+PiranhaPlant::PiranhaPlant(Vector2 position, Vector2 size, Vector2 speed) : Enemy(position, size, speed) {
+    this->position = position;
+    this->originPosition = position;
+    
+    texture = LoadTexture("assets/textures/PiranhaPlant_0.png");
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+
+    textures.push_back(LoadTexture("assets/textures/PiranhaPlant_0.png"));
+    textures.push_back(LoadTexture("assets/textures/PiranhaPlant_1.png"));
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+
+    isRight = false;
+    isDown = false;
+    isDead = false;
+    isPauseCollision = false;
+
+    heightInGround = 0;
+    topBound = size.y + position.y;
+    bottomBound = position.y;
+}
+void PiranhaPlant::update(float deltaTime) {
+    if (isDead) return;
+
+    if (!isPauseCollision) {
+            timer += deltaTime;
+        if (timer >= animationTime) {
+            timer -= animationTime; 
+            currentTextureIndex = (currentTextureIndex + 1) % 2; 
+            texture = textures[currentTextureIndex];
+        }
+
+        if(isDown) {
+            position.y += speed.y * deltaTime;
+            heightInGround += speed.y * deltaTime;
+        }
+        else {
+            position.y -= speed.y * deltaTime;
+            heightInGround -= speed.y * deltaTime;
+        }
+        if(position.y > topBound|| position.y < bottomBound ) {
+            isDown = !isDown;
+        }
+
+        heightInGround = Clamp(heightInGround, 0.0f, size.y);
+    }
+    else {
+        timer += deltaTime;
+        if (timer >= 6 * animationTime) {
+            timer -= 6 * animationTime; 
+            isPauseCollision = false;
+        }
+    }
+}
+
+void PiranhaPlant::render() {
+    if (!isDead) {
+        Rectangle sourceRec = { 0.0f, 0.0f, (float)texture.width, (float)texture.height - heightInGround/size.y * 66};
+        Rectangle destRec = { position.x, position.y, size.x, size.y - heightInGround};
+        Vector2 origin = { 0.0f, 0.0f };
+        DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE);
+    }
+}
+
+void PiranhaPlant::hit() {
+    isDead = true;
+}
+
+
+InversePiranhaPlant::InversePiranhaPlant(Vector2 position) : PiranhaPlant(position) {
+    heightInGround = 66;
+    topBound = position.y + 66;
+    bottomBound = position.y;
+}
+
+InversePiranhaPlant::InversePiranhaPlant(Vector2 position, Vector2 size, Vector2 speed) : PiranhaPlant(position, size, speed) {
+    heightInGround = size.y;
+    topBound = position.y + size.y;
+    bottomBound = position.y;
+    isDown = true;
+}
+void InversePiranhaPlant::update(float deltaTime) {
+    if (isDead) return;
+
+    if (!isPauseCollision) {
+        timer += deltaTime;
+        if (timer >= animationTime) {
+            timer -= animationTime;
+            currentTextureIndex = (currentTextureIndex + 1) % 2;
+            texture = textures[currentTextureIndex];
+        }
+
+        if (isDown) {
+            position.y += speed.y * deltaTime; 
+            heightInGround -= speed.y * deltaTime; 
+        } else {
+            position.y -= speed.y * deltaTime; 
+            heightInGround += speed.y * deltaTime; 
+        }
+        if (position.y <= bottomBound || position.y >= topBound) {
+            isDown = !isDown;
+        }
+    } else {
+        timer += deltaTime;
+        if (timer >= 6 * animationTime) {
+            timer -= 6 * animationTime;
+            isPauseCollision = false;
+        }
+    }
+}
+
+
+void InversePiranhaPlant::render() {
+    if (!isDead) {
+        Rectangle sourceRec = { 
+            0.0f, 
+            0.0f, 
+            (float)texture.width, 
+            -((float)texture.height - heightInGround/size.y * 66)
+        };
+
+        Rectangle destRec = { 
+            originPosition.x, 
+            originPosition.y, 
+            size.x, 
+            size.y - heightInGround
+        };
+
+        Vector2 origin = {0.0f, 0.0f };
+
+        DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE);
+    }
+}
+
+void InversePiranhaPlant::test() {
+    if (IsKeyDown(KEY_A)) {
+        setIsPauseCollision(true);
+    }
+}
+
+ShyGuy::ShyGuy(Vector2 position) : Enemy(position) {
+    this->position = position;
+    this->originPosition = position;
+    
+    texture = LoadTexture("assets/textures/ShyGuy1.png");
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+
+    textures.push_back(LoadTexture("assets/textures/ShyGuy1.png"));
+    textures.push_back(LoadTexture("assets/textures/ShyGuy2.png"));
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+
+    size = { 21, 29 };
+    speed = { 10, 0 };
+    isRight = false;
+    isDown = false;
+    isDead = false;
+    isCollisionTrue = false;
+}
+
+ShyGuy::ShyGuy(Vector2 position, Vector2 size, Vector2 speed) : Enemy(position,size,speed) {
+    this->position = position;
+    this->originPosition = position;
+    
+    texture = LoadTexture("assets/textures/ShyGuy1.png");
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+
+    textures.push_back(LoadTexture("assets/textures/ShyGuy1.png"));
+    textures.push_back(LoadTexture("assets/textures/ShyGuy2.png"));
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+
+    isRight = false;
+    isDown = false;
+    isDead = false;
+    isCollisionTrue = false;
+}
+
+ShyGuy::ShyGuy(Vector2 position, Vector2 size, Vector2 speed, float leftBound, float rightBound, float topBound, float bottomBound) : Enemy(position,size,speed,leftBound,rightBound,topBound,bottomBound) {
+    this->position = position;
+    this->originPosition = position;
+    
+    texture = LoadTexture("assets/textures/ShyGuy1.png");
+    SetTextureFilter(texture, TEXTURE_FILTER_POINT);
+    SetTextureWrap(texture, TEXTURE_WRAP_CLAMP);
+
+    textures.push_back(LoadTexture("assets/textures/ShyGuy1.png"));
+    textures.push_back(LoadTexture("assets/textures/ShyGuy2.png"));
+    SetTextureFilter(textures[0], TEXTURE_FILTER_POINT);
+    SetTextureFilter(textures[1], TEXTURE_FILTER_POINT);
+    SetTextureWrap(textures[0], TEXTURE_WRAP_CLAMP);
+    SetTextureWrap(textures[1], TEXTURE_WRAP_CLAMP);
+
+    isRight = false;
+    isDown = false;
+    isDead = false;
+    isCollisionTrue = false;
+}
+
+void ShyGuy::hit() {
+    if (isCollisionTrue) {
+        isDead = true;
+    }
+}
+
+void ShyGuy::update(float deltaTime) {
+    if (isDead) return;
+
+    if (isRight) {
+        position.x += speed.x * deltaTime;
+    } else {
+        position.x -= speed.x * deltaTime;
+    }
+
+    timer += deltaTime;
+    if (timer >= animationTime) {
+        timer -= animationTime;
+        currentTextureIndex = (currentTextureIndex + 1) % 2;
+        texture = textures[currentTextureIndex];
+    }
+
+    if (position.x < leftBound) {
+        isRight = true;
+    }
+    if (position.x + size.x > rightBound) {
+        isRight = false;
+    }
+
+    if (position.y <= topBound) {
+        isDown = true;
+    }
+
+    if (position.y + size.y >= bottomBound) {
+        isDown = false;
+    }
+}
+
+void ShyGuy::render() {
+    if (!isDead) {
+        if (isRight == false) {
+            DrawTextureEx(texture, position, 0.0f, size.x/21, WHITE);
+        } else {
+            Rectangle sourceRec = { 0, 0, -(float)texture.width, (float)texture.height }; 
+            Rectangle destRec = { position.x, position.y, size.x, size.y };
+            Vector2 origin = { 0.0f, 0.0f };
+            DrawTexturePro(texture, sourceRec, destRec, origin, 0.0f, WHITE);
+        }
+    }
+}
+
