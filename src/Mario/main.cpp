9f3a4ef7b3539e992b7f07480944e5184a9b02d1
Chowkey <lekimchau211@gmail.com>
2024-12-17 23:50:51 +0700

    Init sound and Update sound

--- src/Mario/main.cpp
+++ src/Mario/main.cpp
@@ -1,12 +1,14 @@
 #include "../Application.hpp"
-
+#include "../Sound/Audio.h"
 int main() {
 	SetTargetFPS(60);
     SetConfigFlags(FLAG_MSAA_4X_HINT);
     InitWindow(1024, 768, "Game Window");
+	InitAudioDevice();
     SetWindowState(FLAG_WINDOW_RESIZABLE);
     ResourceManager::GetInstance()->LoadResources();
 
     Application app;
     app.run();
+	CloseAudioDevice();
 }
