7684e625c1c37d3f4d2afb466dd217da9694202d
ntd <lqd.tk22.ngotandat@gmail.com>
2024-12-17 12:19:24 +0700

    .

--- src/Game.cpp
+++ src/Game.cpp
@@ -19,7 +19,7 @@ Game::Game(int characterMenu, int levelMenu)
       level(nullptr)  
 {
     if (levelMenu == 0) {
-        level = factory.CreateLevel(LevelFactory::LEVEL_102);
+        level = factory.CreateLevel(LevelFactory::LEVEL_101);
     }
     else if (levelMenu == 1) {
         level = factory.CreateLevel(LevelFactory::LEVEL_102);
