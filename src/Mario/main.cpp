32332bfafdcbf584f76d2f5e872bf9b2e9b46357
ntd <lqd.tk22.ngotandat@gmail.com>
2024-12-20 21:26:55 +0700

    Add the accelerate for starman

--- src/Mario/main.cpp
+++ src/Mario/main.cpp
@@ -1,88 +1,52 @@
-#include "../Application.hpp"
+//#include "../Application.hpp"
 
-int main() {
-
-	
-    SetTargetFPS(60);
-    InitAudioDevice();
-
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(1024, 768, "Game Window");
-    SetWindowState(FLAG_WINDOW_RESIZABLE);
-    ResourceManager::GetInstance()->LoadResources();
-    Application app;
-    app.run();
-    CloseAudioDevice();
-}
+//int main() {
+//
+//	
+//    SetTargetFPS(60);
+//    InitAudioDevice();
+//
+//    SetConfigFlags(FLAG_MSAA_4X_HINT);
+//    InitWindow(1024, 768, "Game Window");
+//    SetWindowState(FLAG_WINDOW_RESIZABLE);
+//    ResourceManager::GetInstance()->LoadResources();
+//    Application app;
+//    app.run();
+//    CloseAudioDevice();
+//}
 
 // #include "../characters/Enemy.hpp"
 // #include "raylib.h"
 
-// int main() {
-//     SetConfigFlags(FLAG_MSAA_4X_HINT);
-//     InitWindow(1024, 768, "Game Window");
-//     SetWindowState(FLAG_WINDOW_RESIZABLE);
-//     SetTargetFPS(60);
-    
-//     PiranhaPlant koopa({300, 300});
-    
-//     while(!WindowShouldClose()) {
-//         BeginDrawing();
-//         ClearBackground(RAYWHITE);
-//         DrawRectangleLines(300, 300, 42, 58, RED);
-//         // DrawRectangleLines(400, 300, 48, 99, RED);
 
-//         // DrawRectangleLines(goomba.getBoundingBox().x, goomba.getBoundingBox().y, goomba.getBoundingBox().width,  goomba.getBoundingBox().height,BLUE);
-//         // goomba.test();
-//         // goomba.update(0.01f);
-//         // goomba.render();
-//         // DrawRectangleLines(goomba2.getBoundingBox().x, goomba2.getBoundingBox().y, goomba2.getBoundingBox().width,  goomba2.getBoundingBox().height,BLUE);
-
-//         // goomba2.test();
-//         // goomba2.update(0.01f);
-//         // goomba2.render();
-//         DrawLineEx({700,100}, {700,400}, 2.0f, RED);
-//         koopa.update(0.01f);
-//         koopa.render();
-//         if (IsKeyDown(KEY_SPACE)) {
-//             koopa.hit();
-//         }
-//         // shy.update(0.01f);
-//         // shy.render();
-//         EndDrawing();
-        
-//     }
-//     CloseWindow();
-// }
-
-// // #include "../Application.hpp"
+  #include "../Application.hpp"
 
 
-// // #include "../levels/Level.hpp"
-// // int main() {
-// //     const int screenWidth = 1024;
-// //     const int screenHeight = 896;
+  #include "../levels/Level.hpp"
+  int main() {
+      const int screenWidth = 1024;
+      const int screenHeight = 896;
 
-// //     InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
+      InitWindow(screenWidth, screenHeight, "raylib [core] example - basic window");
 
-// //     SetTargetFPS(60);
-// //     LevelFactory& factory = LevelFactory::GetLevelFactory();
-// //     Level* level = factory.CreateLevel(LevelFactory::LEVEL_101);
+      SetTargetFPS(60);
+      LevelFactory& factory = LevelFactory::GetLevelFactory();
+      Level* level = factory.CreateLevel(LevelFactory::LEVEL_101);
 
-// //     while (!WindowShouldClose()) {
+      while (!WindowShouldClose()) {
 
-// //         BeginDrawing();
-// //         ClearBackground(RAYWHITE);
+          BeginDrawing();
+          ClearBackground(RAYWHITE);
 
-// //         if(IsKeyPressed(KEY_P)) {
-// //             level = factory.CreateLevel(LevelFactory::LEVEL_103);
-// //         }
-// //         level->update(GetFrameTime());
+          if(IsKeyPressed(KEY_P)) {
+              level = factory.CreateLevel(LevelFactory::LEVEL_103);
+          }
+          level->update(GetFrameTime());
 
-// //         level->render();
-// //         EndDrawing();
-// //     }
+          level->render();
+          EndDrawing();
+      }
 
-// //     CloseWindow();
-// // }
+      CloseWindow();
+  }
 
