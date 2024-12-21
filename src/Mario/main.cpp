bc3c0c2c93cc4799ac55abc3eb0cdedb03b50043
ngbao12 <ngocbao6422@gmail.com>
2024-12-21 10:11:48 +0700

    test

--- src/Mario/main.cpp
+++ src/Mario/main.cpp
@@ -1,58 +1,58 @@
-#include "../Application.hpp"
-
-int main() {
-
-	
-    SetTargetFPS(60);
-    InitAudioDevice();
-    SetConfigFlags(FLAG_MSAA_4X_HINT);
-    InitWindow(1024, 768, "Game Window");
-    SetWindowState(FLAG_WINDOW_RESIZABLE);
-    ResourceManager::GetInstance()->LoadResources();
-    Application app;
-    app.run();
-    CloseAudioDevice();
-}
-
-// #include "../characters/Enemy.hpp"
-// #include "raylib.h"
+// #include "../Application.hpp"
 
 // int main() {
+
+	
+//     SetTargetFPS(60);
+//     InitAudioDevice();
 //     SetConfigFlags(FLAG_MSAA_4X_HINT);
 //     InitWindow(1024, 768, "Game Window");
 //     SetWindowState(FLAG_WINDOW_RESIZABLE);
-//     SetTargetFPS(60);
-    
-//     PiranhaPlant koopa({300, 300});
-    
-//     while(!WindowShouldClose()) {
-//         BeginDrawing();
-//         ClearBackground(RAYWHITE);
-//         DrawRectangleLines(300, 300, 42, 58, RED);
-//         // DrawRectangleLines(400, 300, 48, 99, RED);
-
-//         // DrawRectangleLines(goomba.getBoundingBox().x, goomba.getBoundingBox().y, goomba.getBoundingBox().width,  goomba.getBoundingBox().height,BLUE);
-//         // goomba.test();
-//         // goomba.update(0.01f);
-//         // goomba.render();
-//         // DrawRectangleLines(goomba2.getBoundingBox().x, goomba2.getBoundingBox().y, goomba2.getBoundingBox().width,  goomba2.getBoundingBox().height,BLUE);
-
-//         // goomba2.test();
-//         // goomba2.update(0.01f);
-//         // goomba2.render();
-//         DrawLineEx({700,100}, {700,400}, 2.0f, RED);
-//         koopa.update(0.01f);
-//         koopa.render();
-//         if (IsKeyDown(KEY_SPACE)) {
-//             koopa.hit();
-//         }
-//         // shy.update(0.01f);
-//         // shy.render();
-//         EndDrawing();
-        
-//     }
-//     CloseWindow();
+//     ResourceManager::GetInstance()->LoadResources();
+//     Application app;
+//     app.run();
+//     CloseAudioDevice();
 // }
+
+#include "../characters/Enemy.hpp"
+#include "raylib.h"
+
+int main() {
+    SetConfigFlags(FLAG_MSAA_4X_HINT);
+    InitWindow(1024, 768, "Game Window");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
+    SetTargetFPS(60);
+    
+    PiranhaPlant koopa({300, 300});
+    
+    while(!WindowShouldClose()) {
+        BeginDrawing();
+        ClearBackground(RAYWHITE);
+        DrawRectangleLines(300, 300, 42, 58, RED);
+        // DrawRectangleLines(400, 300, 48, 99, RED);
+
+        // DrawRectangleLines(goomba.getBoundingBox().x, goomba.getBoundingBox().y, goomba.getBoundingBox().width,  goomba.getBoundingBox().height,BLUE);
+        // goomba.test();
+        // goomba.update(0.01f);
+        // goomba.render();
+        // DrawRectangleLines(goomba2.getBoundingBox().x, goomba2.getBoundingBox().y, goomba2.getBoundingBox().width,  goomba2.getBoundingBox().height,BLUE);
+
+        // goomba2.test();
+        // goomba2.update(0.01f);
+        // goomba2.render();
+        DrawLineEx({700,100}, {700,400}, 2.0f, RED);
+        koopa.update(0.01f);
+        koopa.render();
+        if (IsKeyDown(KEY_SPACE)) {
+            koopa.hit();
+        }
+        // shy.update(0.01f);
+        // shy.render();
+        EndDrawing();
+        
+    }
+    CloseWindow();
+}
 
 // // #include "../Application.hpp"
 
