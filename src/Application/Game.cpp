e5a00ec619484e7e73888724bb7d5c48b43c8b90
ngbao12 <ngocbao6422@gmail.com>
2024-12-23 11:35:16 +0700

    fix win level 3

--- src/Application/Game.cpp
+++ src/Application/Game.cpp
@@ -479,8 +479,6 @@ void Game::handleState() {
             hiddenLevel();
             break;
         case LEVEL_RETURN_MESSAGE::WIN:
-
-            state = LEVEL_RETURN_MESSAGE::RUNNING;
             if (level->GetLevelType() == LevelFactory::LEVEL_103) {
                 level -> reset();
                 saveScore("score.txt");
